0e334a58b52a8494373bf5b0c854c3f906b7851f
Jake <kd7vea@yahoo.com>
2016-03-25 21:10:26 -0600

    second commit

--- logger/ContactTableViewController.swift
+++ logger/ContactTableViewController.swift
@@ -24,12 +24,12 @@ class ContactTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 8
     }
 
     /*
